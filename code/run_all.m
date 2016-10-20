852a5a2984552c4fe97b664ed3e6dcb426d6b845
Chengxin <machengxinchina@gmail.com>
2016-10-20 16:11:14 +0200

    Change simulation times to 100000

--- code/run_all.m
+++ code/run_all.m
@@ -14,9 +14,9 @@ m = 6;
 m0 = 7; % m0 > m
 k= 12;
 p2 = 0.1;
-NUM_SIMULATION = 100; % Number of simulations. Here it should be 100000.
+NUM_SIMULATION = 100000;
 
 %% Plot all the figures
-graph_fig('ER', [N p1], NUM_SIMULATION);
 graph_fig('BA', [N m0 m], NUM_SIMULATION);
+graph_fig('ER', [N p1], NUM_SIMULATION);
 graph_fig('WS', [N k p2], NUM_SIMULATION);
